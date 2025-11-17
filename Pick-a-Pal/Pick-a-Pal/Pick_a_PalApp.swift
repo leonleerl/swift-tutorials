f294451d2cbc85497e76d00488d0f6f18fd93556
Leon Lee <leon.runlongli@gmail.com>
2025-11-17 22:58:03 +0800

    Create dynamic content

---
+++ Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by 李润龙 on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
